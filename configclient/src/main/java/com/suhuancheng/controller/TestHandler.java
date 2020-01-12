d29bb99972e5c09989762587a34ed388c62c2d7a
shc573 <1102921612@qq.com>
2020-01-12 15:26:10 +0800

    修改uerconfig文件名称

--- configclient/src/main/java/com/suhuancheng/controller/TestHandler.java
+++ configclient/src/main/java/com/suhuancheng/controller/TestHandler.java
@@ -1,21 +1,21 @@
 package com.suhuancheng.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
-public class Handler {
+public class TestHandler {
 
     @Value("${server.port}")
     private String port;
 
-    @Value("{server.poo}")
-    private String poo;
     @GetMapping("/index")
     public String index(){
-        return this.port +"-"+ this.poo;
+        return this.port;
     }
 }
